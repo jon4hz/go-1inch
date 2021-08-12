3e47070f51716882172ccb1567692a7725ac5f24
jon4h <admin@psytux.com>
2021-08-12 19:53:45 +0200

    fix type

--- go1inch_test.go
+++ go1inch_test.go
@@ -34,33 +34,16 @@ func TestQuoteWithoutOpts(t *testing.T) {
 	t.Log(res)
 }
 
-func TestQuoteWithtOpts(t *testing.T) {
-	client := go1inch.NewClient()
-
-	res, _, err := client.Quote(context.Background(), "eth",
-		"0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-		"0x6b175474e89094c44da98b954eedeac495271d0f",
-		"100000000000000000000",
-		nil,
-	)
-	if err != nil {
-		t.Error(err)
-	}
-	t.Log(res)
-}
-
 func TestQuoteWithOpts(t *testing.T) {
 	client := go1inch.NewClient()
 	res, _, err := client.Quote(context.Background(), "eth",
-		"0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-		"0x6b175474e89094c44da98b954eedeac495271d0f",
+		"0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+		"0x111111111117dc0aa78b770fa6a738034120c302",
 		"100000000000000000000",
 		&go1inch.QuoteOpts{
 			Fee:             "1",
-			Protocols:       "all",
 			GasPrice:        "1",
 			ComplexityLevel: "2",
-			ConnectorTokens: "0x6b175474e89094c44da98b954eedeac495271d0f",
 			GasLimit:        "10000000",
 			VirtualParts:    "50",
 			Parts:           "50",
@@ -108,22 +91,20 @@ func TestSwapWithOpts(t *testing.T) {
 	client := go1inch.NewClient()
 
 	res, _, err := client.Swap(context.Background(), "eth",
-		"0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-		"0x6b175474e89094c44da98b954eedeac495271d0f",
+		"0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+		"0x111111111117dc0aa78b770fa6a738034120c302",
 		"100000000000000000000",
 		"0x52bc44d5378309ee2abf1539bf71de1b7d7be3b5",
 		1,
 		&go1inch.SwapOpts{
-			Protocols:        "all",
 			DestReceiver:     "0x52bc44d5378309ee2abf1539bf71de1b7d7be3b5",
 			ReferrerAddress:  "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B",
 			Fee:              "1",
 			GasPrice:         "1",
 			BurnChi:          false,
 			ComplexityLevel:  "2",
-			ConnectorTokens:  "0x6b175474e89094c44da98b954eedeac495271d0f",
 			AllowPartialFill: true,
-			DisableEstimate:  false,
+			DisableEstimate:  true,
 			GasLimit:         "10000000",
 			Parts:            "50",
 			VirtualParts:     "50",
@@ -218,22 +199,4 @@ func TestApproveCalldataWithOpts(t *testing.T) {
 		t.Error(err)
 	}
 	t.Log(res)
-}
-
-func TestQuote(t *testing.T) {
-
-	client := NewClient()
-	req := &QuoteReq{
-		FromTokenAddress: "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6",
-		ToTokenAddress:   "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
-		Amount:           "100000000000000000000",
-	}
-	_, code, err := client.Quote(context.Background(), "matic", req)
-	if err != nil {
-		t.Error(err)
-	}
-	if code != 200 {
-		t.Errorf("code returned %d, expected 200", code)
-	}
-
 }
